15d37eed191ea59c251df6b484ba206c6dff49ce
Annie David <E20CSE068@bennett.edu.in>
2023-07-31 00:33:19 +0530

    set up authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
-  return <p>Dashboard page</p>;
+  return (
+    <div>
+      <p>Dashboard page</p>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const LandingPage = () => {
-  return <div>LandingPage(Unprotected)</div>;
+  return (
+    <div>
+      <div>LandingPage(Unprotected)</div>
+      <div>
+        <Link href="/sign-in">
+          <Button>Sign In</Button>
+        </Link>
+        <Link href="/sign-up">
+          <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
